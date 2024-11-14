d1ba390097048129b0bb269426c5d887f8b34b8f
A-0013 <157261625+A-0013@users.noreply.github.com>
2024-11-14 15:49:35 -0500

    code structure

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ def main():
 
 if __name__ == "__main__":
   main()
+
+amount_of_energy = 1
+percentage_of_wind = 0.5
+co2_per_wind = 0.001
+total_co2 = amount_of_energy * percentage_of_wind * co2_per_wind
